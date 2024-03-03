ddb24d444772e482b0c620b52db53a3045570c2c
abdelbar472 <621041@oi.edu.eg>
2024-03-03 07:24:08 +0200

    اهبد

---
+++ clander/student/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .Serializer import *
+from .models import *
+
+from django.shortcuts import render
+
+# Create your views here.
